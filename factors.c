8ecdac219281fa0aa2cf9111c1f197de660548ad
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-03 16:02:25 -0700

    RSA Factoring Challenge

--- factors.c
+++ factors.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
-#include <math.h>
-#include "factors.h"
+#include <stdlib.h>
 
-void factorizeNumber(int number) {
-    int i, factor1, factor2;
+int is_prime(int n)
+{
+	int i;
 
-    for (i = 2; i <= sqrt(number); i++) {
-        if (number % i == 0) {
-            factor1 = i;
-            factor2 = number / i;
-            printf("%d=%d*%d\n", number, factor1, factor2);
-            return;
-        }
-    }
+	if (n <= 1)
+		return 0;
 
-    printf("%d is a prime number\n", number);
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+
+	return 1;
 }
 
-void factorizeNumbersFromFile(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    int number;
+void factorize(int n)
+{
+	int i;
 
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", filename);
-        return;
-    }
+	for (i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			int factor1 = i;
+			int factor2 = n / i;
+			printf("%d=%d*%d\n", n, factor1, factor2);
+			return;
+		}
+	}
 
-    while (fscanf(file, "%d\n", &number) != EOF) {
-        factorizeNumber(number);
-    }
-
-    fclose(file);
+	printf("%d is a prime number\n", n);
 }
+
+void factorize_from_file(const char *filename)
+{
+	FILE *file;
+	int num;
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		printf("Error opening file\n");
+		return;
+	}
+
+	while (fscanf(file, "%d", &num) == 1)
+	{
+		factorize(num);
+	}
+
+	fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: factors <file>\n");
+		return 1;
+	}
+
+	factorize_from_file(argv[1]);
+
+	return 0;
+}
+
--- factors.h
+++
@@ -1,9 +0,0 @@
-#ifndef FACTORS_H
-#define FACTORS_H
-
-#include <stdio.h>
-#include <math.h>
-
-void factorizeNumbersFromFile(const char* filename);
-
-#endif /* FACTORS_H */
